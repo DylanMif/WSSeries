dc7de6a03436a96780a92cba32d09dac9d4b8941
DylanMif <dylan.miftari@etu.univ-savoie.fr>
2024-02-05 14:36:01 +0100

    Mise à jour du modèle, mise en place de la classe partielle

--- WSSeries/Models/EntityFramework/Serie.cs
+++ WSSeries/Models/EntityFramework/Serie.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace WSSeries.Models.EntityFramework
 {
-    [Table("serie")]
+    [Table("serie", Schema = "dylan_schema")]
     public partial class Serie
     {
         [Key]
@@ -26,35 +26,5 @@ namespace WSSeries.Models.EntityFramework
         [Column("network")]
         [StringLength(50)]
         public string? Network { get; set; }
-
-        public Serie(int _serieId, string _titre, string _resume, int _nbsaisons, int nbepisodes, int anneecreation, string _network)
-        {
-            this.Serieid = _serieId;
-            this.Titre = _titre;
-            this.Resume = _resume;
-            this.Nbsaisons = _nbsaisons;
-            this.Nbepisodes = nbepisodes;
-            this.Anneecreation = anneecreation;
-            this.Network = _network;
-        }
-
-        public Serie():this(0, "", "", 0, 0, 0, "") {}
-
-        public override bool Equals(object? obj)
-        {
-            return obj is Serie serie &&
-                   Serieid == serie.Serieid &&
-                   Titre == serie.Titre &&
-                   Resume == serie.Resume &&
-                   Nbsaisons == serie.Nbsaisons &&
-                   Nbepisodes == serie.Nbepisodes &&
-                   Anneecreation == serie.Anneecreation &&
-                   Network == serie.Network;
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(Serieid, Titre, Resume, Nbsaisons, Nbepisodes, Anneecreation, Network);
-        }
     }
 }
---
+++ WSSeries/Models/SeriePart.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WSSeries.Models.EntityFramework
+{
+    public partial class Serie
+    {
+        public override bool Equals(object? obj)
+        {
+            return obj is Serie serie &&
+                   Serieid == serie.Serieid &&
+                   Titre == serie.Titre &&
+                   Resume == serie.Resume &&
+                   Nbsaisons == serie.Nbsaisons &&
+                   Nbepisodes == serie.Nbepisodes &&
+                   Anneecreation == serie.Anneecreation &&
+                   Network == serie.Network;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Serieid, Titre, Resume, Nbsaisons, Nbepisodes, Anneecreation, Network);
+        }
+
+        public Serie(int _serieid, string _titre, string _resume, int nbSaison, int nbEpisode, int anneecreation, string network)
+        {
+            Serieid = _serieid;
+            Titre = _titre;
+            Resume = _resume;
+            Nbsaisons = nbSaison;
+            Nbepisodes = nbEpisode;
+            Anneecreation = anneecreation;
+            Network = network;
+        }
+
+        public Serie():this(1, "", "", 0, 0, 0, "") {}
+    }
+}
